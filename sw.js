fc5af98178a1d6f8c6ebc9fc6674afdaada4fa3a
totraj9 <38199892+totraj9@users.noreply.github.com>
2026-01-27 02:03:32 +0530

    Update sw.js
    not working as offline version

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
+const CACHE_NAME = "pwa-calculator-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("calc-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
